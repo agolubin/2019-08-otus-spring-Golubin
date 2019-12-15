af336fd9b2b8aac50e185057170f958d5700fe06
agolubin <agolubin.diasoft@yandex.ru>
2019-12-15 23:02:27 +0300

    Поправил файл

--- 06/src/main/java/ru/otus/spring06/Spring06Application.java
+++ 06/src/main/java/ru/otus/spring06/Spring06Application.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.h2.tools.Console;
 
 @SpringBootApplication
-public class spring06Application {
+public class Spring06Application {
 
         public static void main(String[] args) throws Exception {
                 Console.main(args);
-                SpringApplication.run(spring06Application.class, args);
+                SpringApplication.run(Spring06Application.class, args);
         }
 
 }
